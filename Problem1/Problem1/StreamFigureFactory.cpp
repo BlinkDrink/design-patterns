891ba9c931e10036791c636db659f0feb274ef55
BlinkDrink <evrolan1404@gmail.com>
2023-11-15 01:22:32 +0200

    Add error handling in streamfigurefactory

--- Problem1/Problem1/StreamFigureFactory.cpp
+++ Problem1/Problem1/StreamFigureFactory.cpp
@@ -18,35 +18,37 @@ namespace Problem1
 {
 	namespace Factories
 	{
-		StreamFigureFactory::StreamFigureFactory(istream& s) : m_stream(s)
-		{
-		}
+		StreamFigureFactory::StreamFigureFactory(istream& s) : m_stream(s) {}
 
-		StreamFigureFactory::StreamFigureFactory(ifstream&& s) : m_file(std::move(s)), m_stream(m_file)
-		{
-		}
+		StreamFigureFactory::StreamFigureFactory(ifstream&& s) : m_file(std::move(s)), m_stream(m_file) {}
 
 		unique_ptr<Figure> StreamFigureFactory::create_figure() const
 		{
 			string type;
-			m_stream >> type;
+			if (!(m_stream >> type))
+				throw invalid_argument("Invalid figure name in file");
 
-			// TODO: Reading input from m_stream may throw (i.e. "rectangle 3.5" this expects another value)
 			if (type == "triangle") {
 				double a, b, c;
-				m_stream >> a >> b >> c;
+				if (!(m_stream >> a >> b >> c))
+					throw invalid_argument("Invalid triangle format in file");
+
 				return make_unique<Triangle>(a, b, c);
 			}
 			else if (type == "rectangle")
 			{
 				double a, b;
-				m_stream >> a >> b;
+				if (!(m_stream >> a >> b))
+					throw invalid_argument("Invalid rectangle format in file");
+
 				return make_unique<Rectangle>(a, b);
 			}
 			else if (type == "circle")
 			{
 				double radius;
-				m_stream >> radius;
+				if (!(m_stream >> radius))
+					throw invalid_argument("Invalid circle format in file");
+
 				return make_unique<Circle>(radius);
 			}
 
